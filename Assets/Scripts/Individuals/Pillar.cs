043c18f09d55dbc25fd575bb18a98de787a3dc1c
Choollol <chrisun8@yahoo.com>
2023-08-01 13:10:44 -0700

    8/1 small changes

--- Assets/Scripts/Individuals/Pillar.cs
+++ Assets/Scripts/Individuals/Pillar.cs
@@ -12,9 +12,9 @@ public class Pillar : MonoBehaviour
 
     private float currentAngle;
     private float radius = 1;
-    private float radiusDecrement = 0.0001f;
+    private float radiusDecrement = 0.0004f;
     private float speed = 1;
-    private float acceleration = 0.005f;
+    private float acceleration = 0.01f;
     private void Start()
     {
         currentAngle = angle + Mathf.PI / 2;
@@ -41,7 +41,7 @@ public class Pillar : MonoBehaviour
 
             transform.localPosition = new Vector3(newX, newY + yOffset, 0);
 
-            if (Mathf.Abs(transform.position.x) > 10 && Mathf.Abs(transform.position.y) > 10)
+            if (Mathf.Abs(transform.localPosition.x) > 5 && Mathf.Abs(transform.localPosition.y) > 5)
             {
                 doRotate = false;
                 gameObject.SetActive(false);
